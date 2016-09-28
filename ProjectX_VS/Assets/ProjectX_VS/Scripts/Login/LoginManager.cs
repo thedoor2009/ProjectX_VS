ddaa815f26bf9b79b79ff45b7ade35696c399180
thedoor2009 <d.jinghui@163.com>
2016-09-27 23:22:26 -0700

    Set up the basic logic of login scene
    Adding two audio files
    Create scene 1 for login event
    Implement basic logic for login event

--- ProjectX_VS/Assets/ProjectX_VS/Scripts/Login/LoginManager.cs
+++ ProjectX_VS/Assets/ProjectX_VS/Scripts/Login/LoginManager.cs
@@ -1,26 +1,60 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LoginManager : MonoBehaviour {
+
+	public float 			FirstPauseTime;
+	public float			PhoneRingTime;
+	public float			PhoneTalkTime;
+
+	public RawImage			BlackScreen;
+	public RawImage			LoginScreen;
 
 	public InputField 		InputField;
 	public string 			LoginPassword;
 	public Text		  		PasswordWrongMessage;
+	public AudioSource      AudioSource;
+	public List<AudioClip>  AudioClipList;
 
+	private string			m_loginState = "";
 	private string 			m_loginPassword = "ebsn3dd9kr";
 
 	void Start () 
 	{
 		m_loginPassword = LoginPassword;
+		m_loginState =  "Black_Screen_No_Sound";
 	}
 
 	void Update () 
 	{
-		if( Input.GetKey(KeyCode.Return))
+		switch(m_loginState)
 		{
-			OnReturn();
+		case "Black_Screen_No_Sound":
+			StartCoroutine(FirstPause(FirstPauseTime));
+			break;
+		case "Black_Screen_Phone_Ring":
+			StartCoroutine(FirstPause(PhoneRingTime));
+			break;
+		case "Black_Screen_Phone_Pick_Up":
+
+			break;
+		case "PC_Start_Up":
+			
+			break;
+		case "PC_Login":
+			if( Input.GetKey(KeyCode.Return))
+			{
+				OnReturn();
+			}
+			break;
+		default:
+			break;
+
 		}
+
+
 	}
 
 	public void OnReturn()
@@ -30,12 +64,31 @@ public class LoginManager : MonoBehaviour {
 		StartCoroutine(CheckPassword());
 	}
 
+	IEnumerator FirstPause( float time)
+	{
+		yield return new WaitForSeconds( time );
+
+		AudioSource.loop = true;
+		AudioSource.clip = AudioClipList[0];
+		AudioSource.Play();
+		m_loginState = "Black_Screen_Phone_Ring";
+	}
+
+	IEnumerator PhoneRing( float time)
+	{
+		yield return new WaitForSeconds( time );
+
+		AudioSource.clip = AudioClipList[1];
+		AudioSource.Play();
+		m_loginState = "Black_Screen_Phone_Pick_Up";
+	}
+
 	IEnumerator CheckPassword()
 	{
 		yield return new WaitForSeconds( 0.2f );
 
 		if( InputField.text == m_loginPassword )
-		{
+		{ 
 			Application.LoadLevel("Scene_OS");
 		}
 		else
